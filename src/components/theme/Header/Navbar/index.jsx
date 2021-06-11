2b590f9ece9e22f026c33d2ff6a102d633551723
Astrid Varga <astrid.varga@gmx.de>
2021-06-11 12:07:10 +0200

    change names

--- src/components/theme/Header/Navbar/index.jsx
+++ src/components/theme/Header/Navbar/index.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <Wrapper as={Container}>
       <Brand as={Link} to="/" theme={theme}>
-        John Doe
+        Astrid Varga
       </Brand>
       <NavbarLinks desktop />
     </Wrapper>
